6e47163622221e577360f76b64b2cbec65bdae5e
ArataKido <shahzod20030905@gmail.com>
2025-06-13 00:52:59 +0500

    changed from asyncpg to psycopg 3, supports both async and sync

--- app/db.py
+++ app/db.py
@@ -6,7 +6,6 @@ from app.config import PostgresConfig
 
 def new_session_maker(psql_config: PostgresConfig) -> async_sessionmaker[AsyncSession]:
     database_uri = f"postgresql+psycopg://{psql_config.user}:{psql_config.password}@{psql_config.host}:{psql_config.port}/{psql_config.database}"
-
     engine = create_async_engine(
         database_uri,
         pool_size=15,
@@ -19,11 +18,6 @@ def new_session_maker(psql_config: PostgresConfig) -> async_sessionmaker[AsyncSe
 
 def get_db():
     pass
-
-# sync_db_url = f"postgresql+psycopg://{psql_config.login}:{psql_config.password}@{psql_config.host}:{psql_config.port}/{psql_config.database}"
-
-# Синхронный генератор сессий для Celery
-# sync_db_url = PostgresConfig.database_url.replace("+asyncpg", "")
 
 def new_sync_session_maker(psql_config: PostgresConfig) -> sessionmaker[Session]:
     database_uri = f"postgresql+psycopg://{psql_config.user}:{psql_config.password}@{psql_config.host}:{psql_config.port}/{psql_config.database}"
@@ -38,21 +32,6 @@ def new_sync_session_maker(psql_config: PostgresConfig) -> sessionmaker[Session]
     )
     return sessionmaker(engine, class_=Session, autoflush=False, expire_on_commit=False)
 
-
-# Синхронное соединение с базой данных для Celery
-# Получаем URL без asyncpg префикса
-# sync_engine = create_engine(
-#     sync_db_url,
-#     echo=False,  # Set to False in production
-#     future=True,
-# )
-
-
-# # Создаем фабрику синхронных сессий для Celery
-# SyncSessionLocal = sessionmaker(
-#     sync_engine,
-#     expire_on_commit=False,
-# )
 def get_db_sync():
     session = new_sync_session_maker()
     try:
